ecf738250ace7e69436d95725f69a3794f4016e9
Changmin Kim <ckd29672@naver.com>
2024-01-11 19:32:06 +0900

    initialize project

---
+++ src/main/java/com/gdschackathon/newyearserver/NewyearserverApplication.java
@@ -0,0 +1,13 @@
+package com.gdschackathon.newyearserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyearserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewyearserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdschackathon/newyearserver/NewyearserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdschackathon.newyearserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewyearserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
